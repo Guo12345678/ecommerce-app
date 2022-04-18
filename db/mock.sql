ea806b1c9fc333011c5359aeefee61de78320d08
Guo12345678 <61332313+Guo12345678@users.noreply.github.com>
2022-04-18 18:17:05 -0400

    Update mock.sql

--- db/mock.sql
+++ db/mock.sql
@@ -18,7 +18,9 @@ insert into Listing (vendor_id, name, price, description) values
     (1, 'Kindle', 29.99,
         'Kindle - With a Built-in Front Light - Black - Ad-Supported'),
     (1, 'Echo Dot', 129.99,
-        'Echo Dot (4th Gen, 2020 release) | Smart speaker with Alexa | Charcoal');
+        'Echo Dot (4th Gen, 2020 release) | Smart speaker with Alexa | Charcoal'),
+    (1, 'Yugioh Frog The Jam Japanese Ultra Rare No Ref 1999.', 52.17, "Condition: Used: An item that has been used previously. See the seller’s listing for full details and description of any imperfections."),
+    (1, 'Ecolution Pure Intentions, 8-Quart, Stainless Steel ', 37.33, "This stockpot is perfect for tackling large meals and so much more!");
 
 insert into ListingImages (listing_id, url) values
     (1, 'https://www.nme.com/wp-content/uploads/2020/06/ps5-credit-sie@2000x1270.jpg'),
@@ -26,7 +28,9 @@ insert into ListingImages (listing_id, url) values
     (3, 'https://m.media-amazon.com/images/I/61vFO3R5UNL._AC_SX679_.jpg'),
     (4, 'https://m.media-amazon.com/images/I/615YaAiA-ML._SX522_.jpg'),
     (5, 'https://m.media-amazon.com/images/I/61LKmORUuXL._AC_SX679_.jpg'),
-    (6, 'https://m.media-amazon.com/images/I/81Eqgg0cvrL._AC_SX679_.jpg');
+    (6, 'https://m.media-amazon.com/images/I/81Eqgg0cvrL._AC_SX679_.jpg'),
+    (7, 'https://i.ebayimg.com/images/g/QIMAAOSwIttiOyuQ/s-l1600.jpg'),
+    (8, 'https://m.media-amazon.com/images/I/61WufZsG7rL._AC_SL1500_.jpg');
 
 insert into Cart (buyer_id, listing_id, qty) values
     (1, 1, 1),
